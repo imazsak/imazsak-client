a9a04e9117c84f60ff8bf1af6b4bee5ae3eddc3a
Ksisu <horkris@gmail.com>
2019-12-05 12:40:08 +0100

    temp use stage backend

--- src/app/imazsak.service.ts
+++ src/app/imazsak.service.ts
@@ -9,6 +9,8 @@ import {AuthService} from './auth.service';
 })
 export class ImazsakService {
 
+  readonly baseDomain = 'https://stage.imazsak.hu';
+
   private getMeCache: Observable<MeData>;
   private listGroupsCache: Observable<GroupListData[]>;
   private listMyPrayersCache: Observable<MyPrayerListData[]>;
@@ -20,7 +22,7 @@ export class ImazsakService {
 
   public getMe(): Observable<MeData> {
     if (!this.getMeCache) {
-      this.getMeCache = this.http.get<MeData>('/api/me').pipe(
+      this.getMeCache = this.http.get<MeData>(`${this.baseDomain}/api/me`).pipe(
         publishReplay(1),
         refCount()
       );
@@ -30,12 +32,12 @@ export class ImazsakService {
 
   public saveMe(data: MeData): Observable<any> {
     this.getMeCache = null;
-    return this.http.post('/api/me', data);
+    return this.http.post(`${this.baseDomain}/api/me`, data);
   }
 
   public listGroups(): Observable<GroupListData[]> {
     if (!this.listGroupsCache) {
-      this.listGroupsCache = this.http.get<GroupListData[]>('/api/groups').pipe(
+      this.listGroupsCache = this.http.get<GroupListData[]>(`${this.baseDomain}/api/groups`).pipe(
         publishReplay(1),
         refCount()
       );
@@ -45,12 +47,12 @@ export class ImazsakService {
 
   public createPrayer(data: CreatePrayerData): Observable<any> {
     this.listMyPrayersCache = null;
-    return this.http.post('/api/prayers', data);
+    return this.http.post(`${this.baseDomain}/api/prayers`, data);
   }
 
   public listMyPrayers(): Observable<MyPrayerListData[]> {
     if (!this.listMyPrayersCache) {
-      this.listMyPrayersCache = this.http.get<MyPrayerListData[]>('/api/prayers').pipe(
+      this.listMyPrayersCache = this.http.get<MyPrayerListData[]>(`${this.baseDomain}/api/prayers`).pipe(
         publishReplay(1),
         refCount()
       );
@@ -59,7 +61,7 @@ export class ImazsakService {
   }
 
   public createFeedback(data: CreateFeedbackData): Observable<any> {
-    return this.http.post('/api/feedback', data);
+    return this.http.post(`${this.baseDomain}/api/feedback`, data);
   }
 
   public listNotifications$(): Observable<NotificationListData[]> {
@@ -67,63 +69,63 @@ export class ImazsakService {
   }
 
   public deleteNotification(id: string): Observable<any> {
-    return this.http.post<any>('/api/me/notifications/delete', {ids: [id]})
+    return this.http.post<any>(`${this.baseDomain}/api/me/notifications/delete`, {ids: [id]})
       .pipe(tap(_ => this.refreshNotifications()));
   }
 
   public readNotification(id: string): Observable<any> {
-    return this.http.post<any>('/api/me/notifications/read', {ids: [id]})
+    return this.http.post<any>(`${this.baseDomain}/api/me/notifications/read`, {ids: [id]})
       .pipe(tap(_ => this.refreshNotifications()));
   }
 
   public listGroupPrayers(groupId: string): Observable<GroupPrayerListData[]> {
-    return this.http.get<GroupPrayerListData[]>(`/api/groups/${groupId}/prayers`);
+    return this.http.get<GroupPrayerListData[]>(`${this.baseDomain}/api/groups/${groupId}/prayers`);
   }
 
   public listGroupMembers(groupId: string): Observable<GroupMemberListData[]> {
-    return this.http.get<GroupMemberListData[]>(`/api/groups/${groupId}/members`)
+    return this.http.get<GroupMemberListData[]>(`${this.baseDomain}/api/groups/${groupId}/members`)
       .pipe(map(members => members.filter(member => !!member.name)));
   }
 
   public sendPray(groupId: string, prayerId: string): Observable<any> {
-    return this.http.post(`/api/groups/${groupId}/prayers/${prayerId}/pray`, {});
+    return this.http.post(`${this.baseDomain}/api/groups/${groupId}/prayers/${prayerId}/pray`, {});
   }
 
   public loadNext10Prayer(groupIds: string[]) {
-    return this.http.post<GroupPrayerListData[]>(`/api/prayers/next-10`, {ids: groupIds});
+    return this.http.post<GroupPrayerListData[]>(`${this.baseDomain}/api/prayers/next-10`, {ids: groupIds});
   }
 
   public closePrayer(data: ClosePrayerRequest): Observable<any> {
     this.listMyPrayersCache = null;
-    return this.http.post('/api/prayers/close', data);
+    return this.http.post(`${this.baseDomain}/api/prayers/close`, data);
   }
 
   public getJoinToGroupToken(groupId: string): Observable<TokenData> {
-    return this.http.post<TokenData>(`/api/groups/${groupId}/create-join-token`, {});
+    return this.http.post<TokenData>(`${this.baseDomain}/api/groups/${groupId}/create-join-token`, {});
   }
 
   public joinToGroup(token: string): Observable<any> {
     this.listGroupsCache = null;
     const data = {token} as TokenData;
-    return this.http.post<any>(`/api/groups/join`, data);
+    return this.http.post<any>(`${this.baseDomain}/api/groups/join`, data);
   }
 
   public pushSubscribe(deviceId: string, subscription: any): Observable<any> {
-    return this.http.post<any>(`/api/me/notifications/push/subscribe`, {deviceId, subscription});
+    return this.http.post<any>(`${this.baseDomain}/api/me/notifications/push/subscribe`, {deviceId, subscription});
   }
 
   public pushUnsubscribe(deviceId: string): Observable<any> {
-    return this.http.post<any>(`/api/me/notifications/push/unsubscribe`, {deviceId});
+    return this.http.post<any>(`${this.baseDomain}/api/me/notifications/push/unsubscribe`, {deviceId});
   }
 
   public pushTest(): Observable<any> {
-    return this.http.post<any>(`/api/me/notifications/push/test`, {});
+    return this.http.post<any>(`${this.baseDomain}/api/me/notifications/push/test`, {});
   }
 
   private refreshNotifications() {
     this.auth.isLoggedIn().subscribe(isLoggenIn => {
       if (isLoggenIn) {
-        this.http.get<NotificationListData[]>('/api/me/notifications').subscribe(notifications => {
+        this.http.get<NotificationListData[]>(`${this.baseDomain}/api/me/notifications`).subscribe(notifications => {
           this.listNotifications.next(notifications);
         });
       }
